d69d39b30a22a4c550e4af0dbd7bac420499da1f
hyein-rachel <hyein_lee@berkeley.edu>
2022-04-10 00:05:25 -0700

    a

--- node-server/index.js
+++ node-server/index.js
@@ -53,3 +53,27 @@ app.post('/post/:postId/comment', (req, res) => {
 app.listen(port, () => {
   console.log(`Block Server listening at http://localhost:${port}`)
 })
+
+const [data, setData] = useState();
+
+function app() {
+  const [data, setData] = useState();
+
+  const getPostsData = () => {
+    axios
+      .get('http://localhost:3002/posts') //THIS IS YOUR URL OF YOUR API
+      .then((data) => setData(Feed.Post)) //PROMISE API, THAT MEANS WHEN YOU GET THE DATA WHAT DO I DO WITH IT
+      .catch((error) => console.log(error)); //ERROR CATCHING IN CASE WE RECEIVE AN ERROR
+  };
+
+  useEffect(() => {
+    getPostsData();
+  }, []);
+
+  return <p>{data ? JSON.stringify(data[0]) : 'loading'}</p>;
+}
+
+axios
+  .post(url, data) // `url` is the url to post to, `data` is the data to send in the body of the HTTP request
+  .then((response) => console.log(response))
+  .catch((error) => console.log(error));
